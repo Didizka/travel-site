50840cf9a16c24070b07c8a717d40737b9570b79
Chingiz <chinjka_m@hotmail.com>
2016-11-23 00:30:23 +0100

    hero section finished

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require("gulp");
+var watch = require("gulp-watch");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var cssvars = require("postcss-simple-vars");
+var cssnested = require("postcss-nested");
+var cssImport = require("postcss-import");
+
+gulp.task("default", function() {
+    console.log("Hooray");
+});
+
+gulp.task("html", function() {
+    console.log("HTMl");
+});
+
+gulp.task("styles", function() {
+    return gulp.src("./app/assets/styles/style.css")
+        .pipe(postcss([cssImport, cssnested, cssvars, autoprefixer]))
+        .pipe(gulp.dest("./app/temp/styles"));
+});
+
+gulp.task("watch", function() {
+    watch("./app/index.html", function() {
+        gulp.start("html"); 
+    });
+    watch("./app/assets/styles/**/*.css", function() {
+        gulp.start("styles");
+    });
+});
